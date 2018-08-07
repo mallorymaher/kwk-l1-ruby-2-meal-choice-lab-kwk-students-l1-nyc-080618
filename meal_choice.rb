c505b540ed879058ff4e0ca0e92b7957c0756ee6
mallorymaher <mallory.maher@comcast.net>
2018-08-07 18:58:25 +0000

    automatically backed up by learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -4,3 +4,14 @@ end
 
 breakfast()
 
+def lunch(food = "grilled cheese")
+  puts "Afternoon is the best time for #{food}!"
+end
+
+lunch()
+
+def dinner(food = "salmon")
+  puts "Evening is the best time for #{food}!"
+end
+
+dinner()
